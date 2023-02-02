be80d286b82c7bc0dd93639614c8d6983fcea525
Edgarunam <etobon96@gmail.com>
2023-02-01 23:59:48 -0600

    Inicio svp1

---
+++ app/src/main/java/dgtic/unam/optica_mexicana/Inicio.kt
@@ -0,0 +1,11 @@
+package dgtic.unam.optica_mexicana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Inicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+    }
+}
