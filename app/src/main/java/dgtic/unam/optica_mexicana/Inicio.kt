3e77f1ac02241bd2728565c8e1744bb826717bf4
Edgarunam <etobon96@gmail.com>
2023-02-02 00:11:16 -0600

    Login e Inicio sv 1

--- app/src/main/java/dgtic/unam/optica_mexicana/Inicio.kt
+++ app/src/main/java/dgtic/unam/optica_mexicana/Inicio.kt
@@ -1,11 +1,18 @@
 package dgtic.unam.optica_mexicana
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+        val botonIngresar = findViewById<Button>(R.id.btnIngresar)
+        botonIngresar.setOnClickListener{
+            val intentOne = Intent(this,MainActivity::class.java)
+            startActivity(intentOne)
+        }
     }
 }
