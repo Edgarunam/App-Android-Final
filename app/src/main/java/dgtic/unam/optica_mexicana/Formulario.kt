21e4b1d3db90c331669ad64ffbae93dd77c8a4ee
Edgarunam <etobon96@gmail.com>
2023-02-04 04:00:43 -0600

    First sp home

---
+++ app/src/main/java/dgtic/unam/optica_mexicana/Formulario.kt
@@ -0,0 +1,11 @@
+package dgtic.unam.optica_mexicana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Formulario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario)
+    }
+}
